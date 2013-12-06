bada0bea1761eba90121e2d7945eeb4c60145e6f
Markus Hubig <mhubig@gmail.com>
2013-12-06 19:09:07 +0100

    Renames some packages.
    Signed-off-by: Markus Hubig <mhubig@gmail.com>

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,6 +4,6 @@ apt-get -y upgrade
 
 apt-get -y update
 apt-get -y install curl
-apt-get -y install vim-nox
-apt-get -y install git-core
+apt-get -y install vim
+apt-get -y install git
 apt-get clean
